d9e4e0230796fae141e754e7e4e930e28c3be927
codeitnav <navya.sriv.1110@gmail.com>
2025-05-09 10:16:28 +0530

    Updated Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,28 +11,32 @@ import curr_img from "../../public/currency_img.png";
 const projects = [
   {
     title: "Expenze - Finance Tracker",
-    description: "Your all-in-one solution to effortlessly manage, analyze, and optimize your personal finances with powerful AI-driven features",
+    description:
+      "Your all-in-one solution to effortlessly manage, analyze, and optimize your personal finances with powerful AI-driven features",
     image: finance,
     link: "https://expenze-finance-tracker.vercel.app/",
     github: "https://github.com/codeitnav/Expenze_Finance_Tracker",
   },
   {
     title: "Scribe - Blogging Website",
-    description: "A minimal and modern blogging website to share stories, insights, and creative content with readers around the world",
+    description:
+      "A minimal and modern blogging website to share stories, insights, and creative content with readers around the world",
     image: blog,
     link: "https://scribeblog.vercel.app/",
     github: "https://github.com/codeitnav/Scribe",
   },
   {
     title: "Password Generator",
-    description: "An interactive web app that lets users create secure, random passwords with customizable options like numbers and special characters, and copy them with a click",
+    description:
+      "An interactive web app that lets users create secure, random passwords with customizable options like numbers and special characters, and copy them with a click",
     image: pass_img,
     link: "https://strong-password-generator-tool.vercel.app/",
     github: "https://github.com/codeitnav/Password-Generator",
   },
   {
     title: "Currency Converter",
-    description: "A responsive and user-friendly web app enabling real-time conversion between 300+ global currencies",
+    description:
+      "A responsive and user-friendly web app enabling real-time conversion between 300+ global currencies",
     image: curr_img,
     link: "https://free-currency-converter-tool.vercel.app/",
     github: "https://github.com/codeitnav/currency-converter",
@@ -47,19 +51,19 @@ const ProjectsSection = () => {
           <CardContainer className="inter-var" key={index}>
             <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full h-auto rounded-xl p-6 border">
               <CardItem
-                translateZ="50"
+                translateZ={50}
                 className="text-xl font-bold text-neutral-600 dark:text-white"
               >
                 {project.title}
               </CardItem>
               <CardItem
                 as="p"
-                translateZ="60"
+                translateZ={60}
                 className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
               >
                 {project.description}
               </CardItem>
-              <CardItem translateZ="100" className="w-full mt-4">
+              <CardItem translateZ={100} className="w-full mt-4">
                 <Image
                   src={project.image}
                   height="1000"
@@ -69,24 +73,30 @@ const ProjectsSection = () => {
                 />
               </CardItem>
               <div className="flex justify-between items-center mt-10">
-                <CardItem
-                  translateZ={20}
-                  as="a"
+                <a
                   href={project.link}
                   target="_blank"
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  rel="noopener noreferrer"
                 >
-                  Link →
-                </CardItem>
-                <CardItem
-                  translateZ={20}
-                  as="a"
+                  <CardItem
+                    translateZ={20}
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Link →
+                  </CardItem>
+                </a>
+                <a
                   href={project.github}
                   target="_blank"
-                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                  rel="noopener noreferrer"
                 >
-                  Github Repo
-                </CardItem>
+                  <CardItem
+                    translateZ={20}
+                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                  >
+                    Github Repo
+                  </CardItem>
+                </a>
               </div>
             </CardBody>
           </CardContainer>
